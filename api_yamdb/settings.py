c94c31445e030ae0bc51a471d5189f080f28530c
Mary Yaschuk <feyaschuk@mail.ru>
2021-11-11 21:36:44 +0300

    add python-decouple to requirements

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -8,9 +8,9 @@ load_dotenv()
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
-SECRET_KEY = config('SECRET_KEY')
+
 
 DEBUG = False
 
