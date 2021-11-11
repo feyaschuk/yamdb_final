24ba8eb586d28f3df0d39f3e8ea56e615cb31f65
Mary Yaschuk <feyaschuk@mail.ru>
2021-11-11 21:27:39 +0300

    fix manage.py

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -2,13 +2,15 @@ import os
 from datetime import timedelta
 
 from dotenv import load_dotenv
+from decouple import config
 
 load_dotenv()
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = os.environ.get('SECRET_KEY')
+# SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
+SECRET_KEY = config('SECRET_KEY')
 
 DEBUG = False
 
